92b0dad3d6a44e8af45ebdd3a3dff6ea99e7e4a1
Kaj van der Valk <kajvandervalk@gmail.com>
2024-04-04 07:21:12 +0200

    Choose own name
    Input field for names that is stored in the query parameters

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-- Choose own name/ 1 hour
-- 
+x Choose own name
+- Reconnect
+- Go back to lobby on disconnect
+- Show current games
+- Creating Lobby with button
+- Back to lobby page when won
--- src/static/lobbies/index.js
+++ src/static/lobbies/index.js
@@ -1,4 +1,6 @@
-
+const queryString = window.location.search
+const urlParams = new URLSearchParams(queryString);
+const userName = urlParams.get('name')
 
 
 fetch('/lobbies').then(response => response.json()).then(data => {
@@ -8,7 +10,7 @@ fetch('/lobbies').then(response => response.json()).then(data => {
         const element = document.createElement('a')
         element.className = 'lobby'
         element.textContent = "Join game"
-        element.href = `../board/index.html?name=kaj&lobby_id=${i}`
-        lobbies.appendChild(element)
+        element.href = `../board/index.html?name=${userName}&lobby_id=${i}`
+        lobbies.prepend(element)
     }
 })
