557b909b2895a019c51d51ca3324ddcbefba0c11
Kaj van der Valk <kajvandervalk@gmail.com>
2024-05-12 20:36:08 +0200

    Rewrite pesten.py
    I didn't like it that there was so much code to do such simpel things, so I rewrote it to just one class.

--- src/pesten.py
+++ src/pesten.py
@@ -1,172 +1,103 @@
-
+from random import shuffle
+from time import sleep
 
 SUITS = ["Harten", "Ruiten", "Schoppen", "Klaver"]
 VALUES = ["Twee", "Drie", "Vier", "Vijf", "Zes", "Zeven", "Acht", "Negen", "Tien", "Boer", "Vrouw", "Heer", "Aas"]
 
 
-class Card:
-    def __init__(self, suit, value) -> None:
-        self.suit = suit
-        self.value = value
-
-    def is_same_suit(self, other_card):
-        if self is other_card:
-            return False
-        return self.suit == other_card.suit or other_card.is_same_suit(other_card)
-
-    def is_same_value(self, other_card):
-        if self is other_card:
-            return False
-        return self.value == other_card.value or other_card.is_same_suit(other_card)
-
-    def __repr__(self) -> str:
-        return self.suit + " " + self.value
+def card(suit, value):
+    # suit: 0-4, value 0-13
+    return suit*13 + value
 
 
-class Deck:
-    def __init__(self, *cards) -> None:
-        self.cards = list(cards)
-
-    def shuffle(self):
-        new_cards = []
-        start = 0
-        end = len(self.cards) - 1
-        while(start < end):
-            new_cards.append(self.cards[end])
-            new_cards.append(self.cards[start])
-            start += 1
-            end -= 1
-        if start == end:
-            new_cards.append(self.cards[start])
-        self.cards = new_cards
-
-    def take(self, index=-1):
-        return self.cards.pop(index)
-    
-    def add(self, card):
-        self.cards.append(card)
-    
-    def is_empty(self):
-        return len(self.cards) == 0
-
-    def __repr__(self) -> str:
-        return str(self.cards)
-    
-    def __len__(self) -> int:
-        return len(self.cards)
-    
-    def __getitem__(self, index):
-        return self.cards[index]
+def card_string(c):
+    return SUITS[c // 13] + " " + VALUES[c % 13]
 
 
-def create_full_deck():
-    return Deck(*[Card(suit, value) for suit in SUITS for value in VALUES])
-    
-
-class Player:
-    def __init__(self, name, *hand) -> None:
-        self.name = name
-        self.hand: list[Card] = list(hand)
-    
-    def give_card(self, card):
-        self.hand.append(card)
-
-    def take_card(self, index):
-        card = self.hand.pop(index)
-        return card
-    
-
-class PlayerGroup:
-    def __init__(self, *players) -> None:
-        self.players: list[Player] = players
-        self.index_current_player = 0
-        self.reverse = False
-
-    def reverse_order(self):
-        self.reverse = not self.reverse
-    
-    def next(self):
-        i = self.index_current_player
-        if not self.reverse:
-            i += 1
-        else:
-            i -= 1
-            i += len(self.players) # Prevent i from becoming negative
-        i %= len(self.players)
-        self.index_current_player = i
-    
-    @property
-    def current_player(self):
-        return self.players[self.index_current_player]
-    
-
-class CannotDraw(Exception):
-    pass
-
-
-class Board:
-    def __init__(self, drawdeck: Deck, playdeck: Deck, players: PlayerGroup) -> None:
-        self.drawdeck = drawdeck
-        self.playdeck = playdeck
-        self.players = players
-
-    @property
-    def top_card(self):
-        return self.playdeck.cards[-1]
-
-    def check(self, index):
-        player = self.players.current_player
-        card = player.hand[index]
-        topcard = self.playdeck[-1]
-        return card.is_same_suit(topcard) or card.is_same_value(topcard)
-
-    def play(self, index: int):
-        player = self.players.current_player
-        card = player.hand.pop(index)
-        self.playdeck.add(card)
-
-    def next(self):
-        self.players.next()
+class Pesten:
+    def __init__(self, player_count: int, hand_count, cards: list) -> None:
+        self.player_count = player_count
+        self.current_player = 0
+        self.draw_stack = cards
+        self.play_stack = [cards.pop()]
+        self.hands = [[] for _ in range(player_count)]
+        self.curr_hand = self.hands[self.current_player]
+        for _ in range(hand_count):
+            for hand in self.hands:
+                hand.append(self.draw_stack.pop())
+        self.has_won = False
 
     def draw(self):
-        drawdeck = self.drawdeck
-        playdeck = self.playdeck
-        if len(drawdeck) + len(playdeck) < 2:
-            raise CannotDraw("Not enough cards on the board to draw one")
-        # Reshuffle if drawdeck is empty
-        if drawdeck.is_empty():
-            topcard = playdeck.take()
-            while(len(playdeck) > 0):
-                card = playdeck.take()
-                drawdeck.add(card)
-            drawdeck.shuffle()
-            playdeck.add(topcard)
-        card = drawdeck.take()
-        self.players.current_player.give_card(card)
+        if len(self.draw_stack) + len(self.play_stack) <= 1:
+            print("Not enough cards on the board to draw. Please play a card")
+            return self.current_player
+        if not self.draw_stack and len(self.play_stack) > 1:
+            top_card = self.play_stack.pop()
+            shuffle(self.play_stack)
+            while self.play_stack:
+                self.draw_stack.append(self.play_stack.pop())
+            self.play_stack.append(top_card)
+        self.curr_hand.append(self.draw_stack.pop())
 
-    def has_won(self):
-        return len(self.players.current_player.hand) == 0
+    def play(self, choose):
+        played_card = self.curr_hand[choose]
+        top_card = self.play_stack[-1]
+        if played_card // 13 == top_card // 13 or played_card % 13 == top_card % 13:
+            self.play_stack.append(self.curr_hand.pop(choose))
+            return True
+        return False
+
+    def next(self):
+        self.current_player += self.player_count + 1 # Make sure it is a positive number
+        self.current_player %= self.player_count
+        self.curr_hand = self.hands[self.current_player]
     
-    def play_turn(self, index: int):
-        # Check if turn was valid by checking if player index changed
-        if index == -1:
+    def play_turn(self, choose) -> int:
+        # Returns index next player
+        if self.has_won:
+            return int(self.current_player)
+        
+        if choose == 0:
             self.draw()
-        elif self.check(index):
-            self.play(index)
-        else:
-            return
-        if not self.has_won():
             self.next()
+        else:
+            choose = choose - 1 # player perspective to game perspective
+            if choose < len(self.curr_hand):
+                if self.play(choose):
+                    if self.curr_hand:
+                        self.next()
+                    else:
+                        self.has_won = True        
+        return int(self.current_player)
+        
 
 
-def create_board(names):
-    player_list = [Player(name) for name in names]
-    players = PlayerGroup(*player_list)
-    playdeck = Deck()
-    drawdeck = create_full_deck()
-    for player in players.players:
-        for _ in range(8):
-            player.give_card(drawdeck.take())
-    playdeck.add(drawdeck.take())
-    board = Board(drawdeck, playdeck, players)
-    return board
+if __name__ == "__main__":
+    while True:
+        cards = [card(suit, value) for suit in range(4) for value in range(13)]
+        shuffle(cards)
+        game = Pesten(4, 8, cards)
+        curr = 0
+        while not game.has_won:
+            print(game.current_player, 'aan zet')
+            print("topcard is", card_string(game.play_stack[-1]))
+            for i, c in enumerate(game.curr_hand, start=1):
+                print(i, card_string(c))
+            print("Wat is je keuze: ")
+            choose = 1
+            while True:
+                new_curr = game.play_turn(choose)
+                if choose == 0:
+                    break
+                if new_curr != curr:
+                    print(choose, "was goed")
+                    curr = new_curr
+                    break
+                print(choose, 'was niet goed')
+                choose += 1
+                if choose >= len(game.curr_hand):
+                    print("kaart trekken")
+                    choose = 0
+
+        else:
+            print(game.current_player, "has won!")
--- tests/test_pesten.py
+++ tests/test_pesten.py
@@ -1,84 +1,89 @@
 import pytest
 
-from pesten import create_board, Board, PlayerGroup, Deck, Player, Card, CannotDraw
-
-TWO_OF_HEARTS = Card('hearts', 'two')
-THREE_OF_HEARTS = Card("hearts", 'three')
-FOUR_OF_HEARTS = Card('hearts', 'four')
-THREE_OF_DIAMONDS = Card('diamonds', 'three')
+from pesten import Pesten, card
 
 
-def test_construct_board():
-    board = create_board(['kaj', 'soy'])
-    assert type(board) == Board
-    assert board.playdeck
-    for player in board.players.players:
-        assert player.hand
+def test_simple_single_player_game():
+    game = Pesten(1, 1, [3, 2, 1, 0])
+    assert game.play_stack == [0]
+    assert game.hands[0] == [1]
+    assert game.draw_stack == [3, 2]
+    game.play(0)
+    game.play(0)
+    assert game.play_stack == [0]
+    assert game.hands[0] == [1, 2, 3]
+    assert game.draw_stack == []
+    game.play(1)
+    game.play(1)
+    game.play(1)
+    assert game.play_stack == [0, 1, 2, 3]
+    assert game.hands[0] == []
+    assert game.draw_stack == []
 
 
-def test_play_board():
-    drawdeck = Deck()
-    playdeck = Deck(FOUR_OF_HEARTS)
-    players = PlayerGroup(
-        Player("player1", TWO_OF_HEARTS, THREE_OF_DIAMONDS),
-        Player("player2", THREE_OF_HEARTS, Card("not", "important"))
-    )
-    board = Board(drawdeck, playdeck, players)
-    board.play_turn(0)
-    assert board.players.index_current_player == 1
-    board.play_turn(0)
-    assert board.players.index_current_player == 0
-    board.play_turn(0)
-    assert board.players.index_current_player == 0
-    assert board.has_won()
+# def test_play_board():
+#     drawdeck = Deck()
+#     playdeck = Deck(FOUR_OF_HEARTS)
+#     players = PlayerGroup(
+#         Player("player1", TWO_OF_HEARTS, THREE_OF_DIAMONDS),
+#         Player("player2", THREE_OF_HEARTS, Card("not", "important"))
+#     )
+#     board = Board(drawdeck, playdeck, players)
+#     board.play_turn(0)
+#     assert board.players.index_current_player == 1
+#     board.play_turn(0)
+#     assert board.players.index_current_player == 0
+#     board.play_turn(0)
+#     assert board.players.index_current_player == 0
+#     assert board.has_won()
 
 
-def test_shuffle_deck_two_cards():
-    deck = Deck(TWO_OF_HEARTS, THREE_OF_HEARTS)
-    deck.shuffle()
-    assert deck.cards == [THREE_OF_HEARTS, TWO_OF_HEARTS]
+# def test_shuffle_deck_two_cards():
+#     deck = Deck(TWO_OF_HEARTS, THREE_OF_HEARTS)
+#     deck.shuffle()
+#     assert deck.cards == [THREE_OF_HEARTS, TWO_OF_HEARTS]
 
 
-def test_shuffle_deck_one_card():
-    deck = Deck(TWO_OF_HEARTS)
-    deck.shuffle()
-    assert deck.cards == [TWO_OF_HEARTS]
+# def test_shuffle_deck_one_card():
+#     deck = Deck(TWO_OF_HEARTS)
+#     deck.shuffle()
+#     assert deck.cards == [TWO_OF_HEARTS]
 
 
-def test_shuffle_deck_three_cards():
-    deck = Deck(TWO_OF_HEARTS, THREE_OF_HEARTS, THREE_OF_DIAMONDS)
-    deck.shuffle()
-    assert deck.cards == [THREE_OF_DIAMONDS, TWO_OF_HEARTS, THREE_OF_HEARTS]
+# def test_shuffle_deck_three_cards():
+#     deck = Deck(TWO_OF_HEARTS, THREE_OF_HEARTS, THREE_OF_DIAMONDS)
+#     deck.shuffle()
+#     assert deck.cards == [THREE_OF_DIAMONDS, TWO_OF_HEARTS, THREE_OF_HEARTS]
 
 
-def test_draw_card():
-    drawdeck = Deck(THREE_OF_DIAMONDS, THREE_OF_HEARTS)
-    playdeck = Deck(FOUR_OF_HEARTS, TWO_OF_HEARTS)
-    players = PlayerGroup(
-        Player("player1", Card("not", "important")),
-        Player("player2", Card("not", "important"))
-    )
-    board = Board(drawdeck, playdeck, players)
-    assert board.top_card == TWO_OF_HEARTS
-    board.play_turn(-1)
-    assert board.players.index_current_player == 1
-    board.play_turn(-1)
-    assert board.players.index_current_player == 0
-    board.play_turn(-1)
-    assert board.players.index_current_player == 1
+# def test_draw_card():
+#     drawdeck = Deck(THREE_OF_DIAMONDS, THREE_OF_HEARTS)
+#     playdeck = Deck(FOUR_OF_HEARTS, TWO_OF_HEARTS)
+#     players = PlayerGroup(
+#         Player("player1", Card("not", "important")),
+#         Player("player2", Card("not", "important"))
+#     )
+#     board = Board(drawdeck, playdeck, players)
+#     assert board.top_card == TWO_OF_HEARTS
+#     board.play_turn(-1)
+#     assert board.players.index_current_player == 1
+#     board.play_turn(-1)
+#     assert board.players.index_current_player == 0
+#     board.play_turn(-1)
+#     assert board.players.index_current_player == 1
 
 
-def test_cannot_draw():
-    drawdeck = Deck(THREE_OF_DIAMONDS)
-    playdeck = Deck(FOUR_OF_HEARTS)
-    players = PlayerGroup(
-        Player("player1", Card("not", "important")),
-        Player("player2", Card("not", "important"))
-    )
-    board = Board(drawdeck, playdeck, players)
-    board.play_turn(-1)
-    assert board.players.index_current_player == 1
-    with pytest.raises(CannotDraw):
-        board.play_turn(-1)
-    assert board.players.index_current_player == 1
+# def test_cannot_draw():
+#     drawdeck = Deck(THREE_OF_DIAMONDS)
+#     playdeck = Deck(FOUR_OF_HEARTS)
+#     players = PlayerGroup(
+#         Player("player1", Card("not", "important")),
+#         Player("player2", Card("not", "important"))
+#     )
+#     board = Board(drawdeck, playdeck, players)
+#     board.play_turn(-1)
+#     assert board.players.index_current_player == 1
+#     with pytest.raises(CannotDraw):
+#         board.play_turn(-1)
+#     assert board.players.index_current_player == 1
     
