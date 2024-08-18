2e0b6a49bd7cbd2447b4c060b2e7402f41a2a535
Kaj van der Valk <kajvandervalk@gmail.com>
2024-08-18 22:12:38 +0200

    tried fixing test
    This test will block when waiting to receive text from the testclient. Have to figure out why.

--- tests/test_server.py
+++ tests/test_server.py
@@ -11,25 +11,22 @@ def test_create_lobby(client):
     assert response.status_code == 200, response.text
 
 
-@pytest.mark.asyncio
-async def test_game_flow(async_client, db):
+def test_game_flow(client, db):
     register_user('kaj', '123', db)
     register_user('soy', '123', db)
     id = create_lobby(LobbyCreate(size=2), db.execute(select(User).where(User.username == 'kaj')).first()[0])
-    with (
-        async_client.websocket_connect(f'lobbies/connect?name_override=kaj&lobby_id={id}') as connection_kaj,
-        async_client.websocket_connect(f'lobbies/connect?name_override=soy&lobby_id={id}') as connection_soy,
-    ):
-        for _ in range(10):
-            message = connection_kaj.receive_text()
-            message = connection_soy.receive_text()
-            connection_kaj.send_text('-1')
-            message = connection_soy.receive_text()
-            message = connection_kaj.receive_text()
-            connection_soy.send_text('-1')
-        message = connection_soy.receive_text()
-        message = connection_kaj.receive_text()
+    connection_kaj = client.websocket_connect(f'lobbies/connect?name_override=kaj&lobby_id={id}')
+    connection_soy = client.websocket_connect(f'lobbies/connect?name_override=soy&lobby_id={id}')
+    connection_kaj.send_text('-1')
+    message = connection_kaj.receive_text()
+    message = connection_soy.receive_text()
+    connection_kaj.send_text('-1')
+    message = connection_soy.receive_text()
+    message = connection_kaj.receive_text()
+    connection_soy.send_text('-1')
+    message = connection_soy.receive_text()
+    message = connection_kaj.receive_text()
 
 
 if __name__ == "__main__":
-    pytest.main([__file__])
+    pytest.main([__file__, '--asyncio-mode=auto'])
