5198a706f5bd1db736c8a714c3fe97809e541ad1
Kaj van der Valk <kajvandervalk@gmail.com>
2024-08-23 20:13:57 +0200

    Continue on lobbies page
    I'd like to get some sort of dev server running that can serve me dummy data

--- src/app/LobbiesPage.jsx
+++ src/app/LobbiesPage.jsx
@@ -1,23 +1,43 @@
 import { useState } from "react"
+import { Link } from "react-router-dom"
+import './LobbiesPage.css'
 
 
-function createLobby(event) {
-    fetch('/lobbies', {
-        method: 'POST',
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: '{"size":2}'
-    }).then(() => location.reload())
+function Lobby(props) {
+    console.log(props)
+    return <Link className="lobby">
+        Join game
+    </Link>
 }
 
+
 function LobbiesPage() {
+    const [lobbies, setLobbies] = useState([])
+    async function createLobby(event) {
+        try {
+            const response = await fetch('/api/lobbies', {
+                method: 'POST',
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: '{"size":2}'
+            })
+
+            if (!response?.ok) {
+                throw new Error("Response not ok")
+            }
+            setLobbies(lobbies => [...lobbies, newLobby])
+        } catch {
+            console.error("Failed to create a new lobby")
+        }
+    }
     return <>
         <header>Lobbies</header>
         <div className="lobbies">
             <div id="lobby-list">
+                {lobbies.map((lobby, i) => <Lobby key={i} {...lobby}/>)}
             </div>
-            <button id="new-lobby-button" onclick={createLobby}>Create new</button>
+            <button id="new-lobby-button" onClick={createLobby}>Create new</button>
         </div>
     </>
 }
