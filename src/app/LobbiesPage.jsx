1dda44d193bf01375e49f85ba38389cab3fa201f
Kaj van der Valk <kajvandervalk@gmail.com>
2024-09-28 09:54:39 +0200

    adjust the frontend on the new response model
    operations to the lobbies endpoints return a consistent response. The React components can now efficiently rerender on every operation. Not needed to fetch after every operation.

--- src/app/LobbiesPage.jsx
+++ src/app/LobbiesPage.jsx
@@ -7,23 +7,8 @@ import { AuthContext } from "./AuthProvider"
 const LobbiesContext = createContext()
 
 
-async function getLobbies(accessToken) {
-    const response = await fetch("/api/lobbies", {
-        method: 'get',
-        headers: {
-            "Authorization": accessToken,
-            "Content-Type": "application/json",
-        },
-    })
-    if (!response.ok) {
-        return Promise.reject(response.text)
-    }
-    return response.json()
-}
-
-
 async function postLobby(size, accessToken) {
-    fetch('/api/lobbies', {
+    const response = await fetch('/api/lobbies', {
         method: 'POST',
         headers: {
             "Authorization": accessToken,
@@ -31,109 +16,77 @@ async function postLobby(size, accessToken) {
         },
         body: JSON.stringify({size: size})
     })
-        .then(response => {
-            if (!response?.ok) {
-                return response.reject("Response not ok")
-            }
-            return response.json()
-        })
+    if (!response.ok) {
+        throw new Error("Failed to create a lobby")
+    }
+    return response.json()
 }
 
 
 function LobbiesProvider({children}) {
     const [lobbies, setLobbies] = useState([])
-    const [fetching, setFetching] = useState(true)
     const [accessToken, setAccessToken] = useContext(AuthContext)
 
-    useEffect(() => {
-        if (!fetching) return
-        getLobbies(accessToken)
-            .then(lobbies => {
-                console.log("setting lobbies")
-                console.log(lobbies)
-                setLobbies(lobbies)
-            })
-            .catch(error => {
-                console.error(error)
-            })
-            .finally(() => {
-                setFetching(false)
-            })
-    }, [fetching])
 
-    function fetchLobbies() {
-        setFetching(true)
+    async function getLobbies() {
+        const response = await fetch("/api/lobbies", {
+            method: 'get',
+            headers: {
+                "Authorization": accessToken,
+                "Content-Type": "application/json",
+            },
+        })
+        setLobbies(await response.json())
     }
 
-    return <LobbiesContext.Provider value={[lobbies, fetchLobbies]}>
+    async function createLobby(size) {
+        const lobbies = await postLobby(size, accessToken)
+        setLobbies(lobbies)
+    }
+
+    async function deleteLobby(id) {
+        const response = await fetch(`/api/lobbies?id=${id}`, {
+            method: 'delete',
+        })
+        setLobbies(await response.json())
+    }
+    
+    useEffect(() => {
+        getLobbies()
+    }, [])
+
+
+    return <LobbiesContext.Provider value={{lobbies, getLobbies, createLobby, deleteLobby}}>
         {children}
     </LobbiesContext.Provider>
 }
 
-function useLobbies() {
-    const [lobbies, fetchLobbies] = useContext(LobbiesContext)
-    const [posting, setPosting] = useState(false)
-    const [deleting, setDeleting] = useState(false)
-    const [accessToken, setAccessToken] = useContext(AuthContext)
-
-
-    const postingSize = useRef()
-    useEffect(() => {
-        if (!posting) return
-        postLobby(postingSize, accessToken)
-            .then(newLobby => {
-                console.log(newLobby)
-                fetchLobbies()
-            })
-            .finally(() => {
-                setPosting(false)
-            })
-    }, [posting])
-
-    const deleteId = useRef()
-    useEffect(() => {
-        if (!deleting) return
-        fetch(`/api/lobbies?id=${deleteId.current}`, {
-            method: 'delete',
-        }).then(() => {
-            fetchLobbies()
-        }).finally(() => {
-            setDeleting(false)
-        })
-    }, [deleting])
-    
-    function createLobby(size) {
-        postingSize.current = size
-        setPosting(true)
-    }
-
-    function deleteLobby(id) {
-        deleteId.current = id
-        setDeleting(true)
-    }
-    return {lobbies, fetchLobbies, createLobby, deleteLobby}
-}
 
 function Lobby({id, size, creator}) {
-    const api = useLobbies()
+    const lobbies = useContext(LobbiesContext)
+    const [deleting, setDeleting] = useState(false)
+    useEffect(() => {
+        if (!deleting) return
+        lobbies.deleteLobby(id)
+    }, [deleting])
     return <>
         <Link className="lobby">
-            Join game
+            Join game {id}
         </Link>
-        <button onClick={() => api.deleteLobby(id)}>Delete</button>
+        <button onClick={() => setDeleting(true)}>Delete</button>
     </>
 }
 
 
 function LobbyList() {
-    const api = useLobbies()
+    const lobbies = useContext(LobbiesContext)
     return <>
         <header>Lobbies</header>
         <div className="lobbies">
             <div id="lobby-list">
-                {api.lobbies.map((lobby, i) => <Lobby key={i} {...lobby}/>)}
+                {lobbies.lobbies.map((lobby, i) => <Lobby key={lobby.id} {...lobby}/>)}
             </div>
-            <button id="new-lobby-button" onClick={() => api.createLobby(2)}>Create new</button>
+            <button id="new-lobby-button" onClick={() => lobbies.createLobby(2)}>Create new</button>
         </div>
     </>
 }
