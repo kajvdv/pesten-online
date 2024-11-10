940aa73da075c8beb8091ed6b69df645c49b4dee
Kaj van der Valk <kajvandervalk@gmail.com>
2024-11-10 08:29:17 +0100

    Rename vite folder

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: './src/app',
+  root: './src/vite',
   publicDir: resolve(__dirname, 'public'),
   build: {
     watch: {},
