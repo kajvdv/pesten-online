7083c17bcff5646125b249dfc9a26185d7319749
Kaj van der Valk <kajvandervalk@gmail.com>
2025-03-08 21:51:39 +0100

    don't rely on uvicorn for logs

--- server/__main__.py
+++ server/__main__.py
@@ -4,5 +4,10 @@ from dotenv import load_dotenv
 load_dotenv(".env")
 load_dotenv(".env.local")
 
+import logging
+
 import uvicorn
+
+logger = logging.basicConfig(level=logging.DEBUG)
+
 uvicorn.run("server.server:app", reload='--reload' in sys.argv)
